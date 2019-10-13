91ee963e0dbc1389b44b79dc13f721b371422f57
sqoshi <sqoshi@github.com>
2019-10-13 13:11:25 +0200

    lets do it

---
+++ list-01/src/App.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("podaj text");
+        Scanner scanner = new Scanner(System.in);
+        String T = scanner.next();
+        new Automat(T,function,)
+    }
+}

4b35205f8477996e4ed6727df46ecff8e6ae438d
Piotr Popis <piotrpopis98@icloud.com>
2019-11-10 01:12:04 +0100

    x

--- list02/exercise2/scan.c
+++ list02/exercise2/scan.c
@@ -352,8 +352,8 @@ static void yynoreturn yy_fatal_error ( const char* msg  );
 	(yy_hold_char) = *yy_cp; \
 	*yy_cp = '\0'; \
 	(yy_c_buf_p) = yy_cp;
-#define YY_NUM_RULES 2
-#define YY_END_OF_BUFFER 3
+#define YY_NUM_RULES 3
+#define YY_END_OF_BUFFER 4
 /* This struct is not used in this scanner,
    but its presence is necessary. */
 struct yy_trans_info
@@ -361,10 +361,10 @@ struct yy_trans_info
 	flex_int32_t yy_verify;
 	flex_int32_t yy_nxt;
 	};
-static const flex_int16_t yy_accept[14] =
+static const flex_int16_t yy_accept[18] =
     {   0,
-        0,    0,    3,    2,    2,    0,    0,    0,    0,    0,
-        0,    1,    0
+        0,    0,    4,    3,    3,    0,    0,    0,    0,    0,
+        0,    0,    0,    0,    1,    2,    0
     } ;
 
 static const YY_CHAR yy_ec[256] =
@@ -404,30 +404,34 @@ static const YY_CHAR yy_meta[6] =
         1,    2,    3,    1,    4
     } ;
 
-static const flex_int16_t yy_base[21] =
+static const flex_int16_t yy_base[26] =
     {   0,
-       14,   13,   16,   19,    0,    0,    0,   12,   11,    0,
-        0,   19,   19,    0,   11,    9,    8,    4,    7,    5
+       24,   23,   26,   29,    0,    0,    0,   22,   21,   20,
+       19,    0,    0,    0,   29,   29,   29,    5,   19,   17,
+       16,    9,   13,   15,    0
     } ;
 
-static const flex_int16_t yy_def[21] =
+static const flex_int16_t yy_def[26] =
     {   0,
-       14,   14,   13,   13,   15,   16,   17,   18,   18,   19,
-       20,   13,    0,   13,   13,   13,   13,   13,   13,   13
+       18,   18,   17,   17,   19,   20,   21,   22,   22,   23,
+       23,   17,   24,   25,   17,   17,    0,   17,   17,   17,
+       17,   17,   17,   17,   17
     } ;
 
-static const flex_int16_t yy_nxt[25] =
+static const flex_int16_t yy_nxt[35] =
     {   0,
-        4,    4,    4,    4,    9,    9,    9,    9,   12,   11,
-        8,    7,    6,   10,   10,   13,    5,    5,    3,   13,
-       13,   13,   13,   13
+       17,   17,   14,   16,   15,    4,    4,    4,    4,    9,
+        9,    9,    9,   11,   11,   11,   11,   14,    8,    7,
+        6,   13,   12,   10,   10,   17,    5,    5,    3,   17,
+       17,   17,   17,   17
     } ;
 
-static const flex_int16_t yy_chk[25] =
+static const flex_int16_t yy_chk[35] =
     {   0,
-       14,   14,   14,   14,   18,   18,   18,   18,   20,   19,
-       17,   16,   15,    9,    8,    3,    2,    1,   13,   13,
-       13,   13,   13,   13
+        0,    0,   12,   25,   12,   18,   18,   18,   18,   22,
+       22,   22,   22,   23,   23,   23,   23,   24,   21,   20,
+       19,   11,   10,    9,    8,    3,    2,    1,   17,   17,
+       17,   17,   17,   17
     } ;
 
 static yy_state_type yy_last_accepting_state;
@@ -450,8 +454,8 @@ char *yytext;
 #include <string.h>
 int yywrap();
 int yylex();
-#line 454 "scan.c"
-#line 455 "scan.c"
+#line 458 "scan.c"
+#line 459 "scan.c"
 
 #define INITIAL 0
 
@@ -670,7 +674,7 @@ YY_DECL
 	{
 #line 10 "zad2.l"
 
-#line 674 "scan.c"
+#line 678 "scan.c"
 
 	while ( /*CONSTCOND*/1 )		/* loops until end-of-file is reached */
 		{
@@ -697,13 +701,13 @@ yy_match:
 			while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 				{
 				yy_current_state = (int) yy_def[yy_current_state];
-				if ( yy_current_state >= 14 )
+				if ( yy_current_state >= 18 )
 					yy_c = yy_meta[yy_c];
 				}
 			yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
 			++yy_cp;
 			}
-		while ( yy_base[yy_current_state] != 19 );
+		while ( yy_base[yy_current_state] != 29 );
 
 yy_find_action:
 		yy_act = yy_accept[yy_current_state];
@@ -734,11 +738,17 @@ YY_RULE_SETUP
 ;
 	YY_BREAK
 case 2:
+/* rule 2 can match eol */
 YY_RULE_SETUP
 #line 12 "zad2.l"
+;
+	YY_BREAK
+case 3:
+YY_RULE_SETUP
+#line 13 "zad2.l"
 ECHO;
 	YY_BREAK
-#line 742 "scan.c"
+#line 752 "scan.c"
 case YY_STATE_EOF(INITIAL):
 	yyterminate();
 
@@ -1035,7 +1045,7 @@ static int yy_get_next_buffer (void)
 		while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 			{
 			yy_current_state = (int) yy_def[yy_current_state];
-			if ( yy_current_state >= 14 )
+			if ( yy_current_state >= 18 )
 				yy_c = yy_meta[yy_c];
 			}
 		yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
@@ -1063,11 +1073,11 @@ static int yy_get_next_buffer (void)
 	while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 		{
 		yy_current_state = (int) yy_def[yy_current_state];
-		if ( yy_current_state >= 14 )
+		if ( yy_current_state >= 18 )
 			yy_c = yy_meta[yy_c];
 		}
 	yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
-	yy_is_jam = (yy_current_state == 13);
+	yy_is_jam = (yy_current_state == 17);
 
 		return yy_is_jam ? 0 : yy_current_state;
 }
@@ -1743,7 +1753,7 @@ void yyfree (void * ptr )
 
 #define YYTABLES_NAME "yytables"
 
-#line 12 "zad2.l"
+#line 13 "zad2.l"
 
 
 

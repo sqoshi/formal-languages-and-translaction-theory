08aa0de31412d5a7aba734c41f6fb25b4327826b
Piotr Popis <piotrpopis98@icloud.com>
2019-12-01 17:53:47 +0100

    i want just to finish this

---
+++ list03/method.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cmath>
+
+
+#include "method.hpp"
+
+int add(int a, int b){return a + b;}
+int sub(int a, int b){return a - b;}
+int multiply(int a, int b){return a * b;}
+int div(int a, int b){return floor(a / b);}
+int mypow(int a, int b){return pow(a,b)}
+int modulo(int a, int b){return a % b;}
---
+++ list03/method.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+int add(int a, int b);
+int sub(int a, int b);
+int multiply(int a, int b);
+int div(int a, int b);
+int mypow(int a, int b);
+int modulo(int a, int b);
